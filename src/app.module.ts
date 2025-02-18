efd0a8d597f5e702c04e4a7dc688e1055e5d9456
Diego Delgado <diego.960705@gmail.com>
2025-02-18 12:08:00 +0100

    main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    ProductsModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
